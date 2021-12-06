7013e3abbf380ddbd025db93bee637bdacbf9d8d
akmatoff <akmatoff.exe@gmail.com>
2021-12-06 20:32:21 +0600

    UPDATE: get quotes

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -1,16 +1,31 @@
 import { AiOutlineTwitter, RiDoubleQuotesR } from "react-icons/all";
+import { useState, useEffect } from "react";
+
+import quotes from "../assets/quotes.json";
 import "./QuoteBox.css";
 
 function QuoteBox() {
+  const [quote, setQuote] = useState({});
+
+  useEffect(() => {
+    updateQuote();
+  }, []);
+
+  const updateQuote = () => {
+    setQuote(quotes[Math.floor(Math.random() * quotes.length)]);
+  };
+
   return (
     <div className="flex-column" id="quote-box">
       <RiDoubleQuotesR className="quote-icon" />
       <div className="flex-column centered" id="text">
-        Лучше жопа в тепле, чем тепло в жопе.
+        {quote && quote.quote}
       </div>
-      <div id="author">- Темирлан Джолдошбеков</div>
+      <div id="author">- {quote && quote.author}</div>
       <div className="flex-row" id="buttons-container">
-        <button id="new-quote">New Quote</button>
+        <button id="new-quote" onClick={updateQuote}>
+          New Quote
+        </button>
         <a
           id="tweet-quote"
           href="https://twitter.com/intent/tweet"
